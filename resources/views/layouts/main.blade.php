d3344f0ef36d905d5e6acab5b877e57455699bb4
Ish Sookun <ish.sookun@lasentinelle.mu>
2024-08-12 18:42:30 +0400

    Added Alpine.js

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,7 +9,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-        @vite('resources/css/app.css')
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
         @yield('content')
