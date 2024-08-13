3da04038ee2ba4ce0433021f5f867db5a34e8d9f
Ish Sookun <ish.sookun@lasentinelle.mu>
2024-08-13 23:23:56 +0400

    Hompage dynamic

---
+++ app/Models/Event.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $connection = 'backoffice';
+    protected $table = 'events';
+}
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
+    $events = App\Models\Event::all();
+    return view('index', compact('events'));
 });
